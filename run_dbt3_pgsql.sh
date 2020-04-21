13f67551e34ebdedf61a767a40ebc36aa6feb6c9
Wei WEI <dotweiba@gmail.com>
2020-04-21 21:12:49 +0000

    Fixed.

--- run_dbt3_pgsql.sh
+++ run_dbt3_pgsql.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt-get install build-essential autoconf
+sudo apt-get install -y  build-essential autoconf
 ./autogen.sh
 ./configure --with-postgresql
 make && sudo make install
-sudo apt install postgresql libpq-dev r-base bc sysstat
+sudo apt install -y postgresql libpq-dev r-base bc sysstat
 sudo cp -R ../dbt3/ /opt/
 sudo chown -R postgres:postgres /opt/dbt3
 sudo -u postgres /bin/bash -c "source /opt/dbt3/dbt3_pgsql_profile && rm -rf /tmp/results && dbt3-run-workload -a pgsql -f 1 -o /tmp/results"
